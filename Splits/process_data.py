0381adb0cbe7317c8e425fc6e027730e68fb688a
Sam Denton <smd2202@columbia.edu>
2021-12-26 19:21:36 -0800

    Fix bug with overwriting results.

--- Splits/process_data.py
+++ Splits/process_data.py
@@ -3,6 +3,7 @@ import numpy as np
 import argparse
 import glob
 import os
+import shutil
 
 GAME_RESULT_COLS = ['PTS', 'REB', 'AST', 'GS40', 'OFF', 'DEF',
        'STL', 'BL', 'TOV', 'BLA', 'PF', '2P', '2P%', '3P', '3P%', 'FT', 'FT%',
@@ -74,7 +75,7 @@ def process_file(filename):
         temp_df = temp_df.join(pd.DataFrame(np.repeat(player_name, len(temp_df)), columns=['PLAYER'], index = temp_df.index))
         split_dfs[splits[i]] = temp_df
 
-    return split_dfs
+    return (player_name, split_dfs)
 
 def process_directory(directory, players):
     """
@@ -106,16 +107,27 @@ def process_directory(directory, players):
     
     return all_dfs
 
-def analyze_player(split_dict, split_category, output_directory):
+def analyze_player(player_name, split_dict, split_category, output_directory):
     all_results = []
+    
+
+    outdir = output_directory.replace('/', '')
+    if len(glob.glob(outdir)) == 0:
+        os.mkdir(outdir)
+    else:
+        if os.path.exists(outdir + '/' + player_name + '.txt'):
+            os.remove(outdir + '/' + player_name + '.txt')
+
     if split_category == 'all':
+        text_string = player_name.upper() + ' RESULTS: \n\n\n'
         for key in split_dict.keys():
-            result = analyze_split(key, split_dict[key], output_directory)
+            result = analyze_split(key, split_dict[key], output_directory, text_string)
             if len(result) > 0:
                 all_results.append(result)
 
     elif split_category in split_dict.keys():
-        result = analyze_split(split_category, split_dict[split_category], output_directory)
+        text_string = player_name.upper() + ' RESULTS: \n\n\n'
+        result = analyze_split(split_category, split_dict[split_category], output_directory, text_string)
         if len(result) > 0:
             all_results.append(result)
 
@@ -124,12 +136,8 @@ def analyze_player(split_dict, split_category, output_directory):
 
     return all_results
 
-def analyze_split(split_name, split_df, outdir = 'Results'):
+def analyze_split(split_name, split_df, outdir, text_string):
     player_name = split_df['PLAYER'].unique()[0]
-
-    outdir = outdir.replace('/', '')
-    if len(glob.glob(outdir)) == 0:
-        os.mkdir(outdir)
 
     cols_of_interest = []
     for col in GAME_RESULT_COLS:
@@ -143,7 +151,6 @@ def analyze_split(split_name, split_df, outdir = 'Results'):
     means = np.mean(split_df[cols_of_interest].values, axis = 0)
 
     results = {}
-    text_string = player_name.upper() + ' RESULTS: \n\n\n'
     for index, row in split_df.iterrows():
         split = row['SPLIT']
         
@@ -158,7 +165,7 @@ def analyze_split(split_name, split_df, outdir = 'Results'):
             text_string += str(pd.DataFrame(anomalies, index = ["anomaly", "avg.", "std. dev."])) + "\n"
             results[split] = anomalies
 
-    with open('{}/{}.txt'.format(outdir, player_name), 'w') as f:
+    with open('{}/{}.txt'.format(outdir, player_name), 'a') as f:
         f.write(text_string)
         f.close()
 
@@ -176,7 +183,7 @@ if __name__ == "__main__":
 
     basketball_dicts = process_directory(args.directory, args.players)
 
-    for split_dict in basketball_dicts:
-        analyze_player(split_dict, args.split_category, args.outdir)
+    for (player_name, split_dict) in basketball_dicts:
+        analyze_player(player_name, split_dict, args.split_category, args.outdir)
 
     print("Finished analyzing! Output in {}".format(args.outdir))
